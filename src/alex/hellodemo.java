c61d0a01e652c6774bbb27ff169962a75b0e6cee
Frank <Frank@Alexfranklin0406>
2023-11-17 13:21:13 +0530

    hlo

--- src/alex/hellodemo.java
+++ src/alex/hellodemo.java
@@ -1,10 +1,16 @@
 package alex;
 
+
 public class hellodemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Point p1 = new point(x=5,y=7);
+Point p2 =p1;
+System.out.Println(p2);
+p1.x=7;
+System.out.Println(p1);
+System.out.Println(p2);
 	}
 
 }
