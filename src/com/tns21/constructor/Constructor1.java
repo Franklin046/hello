6497c88cbd2b64e6c613b758183661a313f71bad
Frank <Frank@Alexfranklin0406>
2023-11-17 23:40:32 +0530

    Constructor

---
+++ src/com/tns21/constructor/Constructor1.java
@@ -0,0 +1,27 @@
+package com.tns21.constructor;
+import java.util.Scanner;
+public class Constructor1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String name, city;
+		int id;
+		System.out.println("enter customer id");
+		id=sc.nextInt();
+		sc.nextLine();
+		System.out.println("enter customer name");
+		name=sc.next();
+		System.out.println("enter customer city");
+		city=sc.next();
+		Customer c1=new Customer();
+	    System.out.println(c1);
+	    Customer c= new Customer(name,id,city);
+        c.getCustomerName();
+        c.getCustomerId();
+        c.getCustomerCity();
+	    System.out.println(c);
+		
+		
+		
+	}
+}
